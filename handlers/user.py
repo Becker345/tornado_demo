1b980ea903f893c21154dfbf7f6c672b9f5172de
Becker <hanbq@inspur.com>
2016-05-17 09:10:26 +0800

    修改继承，解决了bug

--- handlers/user.py
+++ handlers/user.py
@@ -5,7 +5,7 @@ import tornado.web
 import methods.readdb as mrd
 
 
-class UserHandler(tornado.web.RedirectHandler):
+class UserHandler(tornado.web.RequestHandler):
     def get(self):
         username = self.get_argument("user")
         user_infos = mrd.select_table(table="authors", column="*", condition="name", value=username)
